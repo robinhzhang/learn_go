f905e0693d69e28f4e65b57979a1548678e0fd18
“robinhzhang” <1137831420@qq.com>
2022-05-07 22:17:04 +0800

    --学习了switch

--- if_switch.go
+++ if_switch.go
@@ -1,2 +1,106 @@
 package main
 
+import "fmt"
+
+func main() {
+	/* 定义局部变量 */
+	var a int = 10
+
+	/* 使用 if 语句判断布尔表达式 */
+	if a < 20 {
+		/* 如果条件为 true 则执行以下语句 */
+		fmt.Printf("a 小于 20\n")
+	}
+	fmt.Printf("a 的值为 : %d\n", a)
+
+	//变体 将声明语句融入其中
+	if num := 10; num%2 == 0 { //checks if number is even
+		fmt.Println(num, "is even")
+	} else {
+		fmt.Println(num, "is odd")
+	}
+	/*
+		而如果switch没有表达式，它会匹配true
+
+		Go里面switch默认相当于每个case最后带有break，
+		匹配成功后不会自动向下执行其他case，
+		而是跳出整个switch, 但是可以使用fallthrough强制执行后面的case代码。
+	*/
+	/* 定义局部变量 */
+
+	var grade string = "B"
+	var marks int = 90
+
+	switch marks {
+	case 90:
+		grade = "A"
+	case 80:
+		grade = "B"
+	case 50, 60, 70:
+		grade = "C" //case 后可以由多个数值
+	default:
+		grade = "D"
+	}
+
+	switch {
+	case grade == "A":
+		fmt.Printf("优秀!\n")
+	case grade == "B", grade == "C":
+		fmt.Printf("良好\n")
+	case grade == "D":
+		fmt.Printf("及格\n")
+	case grade == "F":
+		fmt.Printf("不及格\n")
+	default:
+		fmt.Printf("差\n")
+	}
+	fmt.Printf("你的等级是 %s\n", grade)
+
+	//fallthrough 贯通
+	switch x := 5; x {
+	default:
+		fmt.Println(x)
+	case 5:
+		x += 10
+		fmt.Println(x)
+		fallthrough
+	case 6:
+		x += 20
+		fmt.Println(x)
+
+	}
+	// 省略swich后面的表达式 自动匹配为true
+	num := 75
+	switch { // expression is omitted
+	case num >= 0 && num <= 50:
+		fmt.Println("num is greater than 0 and less than 50")
+	case num >= 51 && num <= 100:
+		fmt.Println("num is greater than 51 and less than 100")
+	case num >= 101:
+		fmt.Println("num is greater than 100")
+	}
+	/*
+		switch的注意事项
+
+		case后的常量值不能重复
+		case后可以有多个常量值
+		fallthrough应该是某个case的最后一行。如果它出现在中间的某个地方，编译器就会抛出错误。
+	*/
+	// type switch
+	var x interface{}
+
+	switch i := x.(type) {
+	case nil:
+		fmt.Printf(" x 的类型 :%T", i)
+	case int:
+		fmt.Printf("x 是 int 型")
+	case float64:
+		fmt.Printf("x 是 float64 型")
+	case func(int) float64:
+		fmt.Printf("x 是 func(int) 型")
+	case bool, string:
+		fmt.Printf("x 是 bool 或 string 型")
+	default:
+		fmt.Printf("未知型")
+	}
+}
