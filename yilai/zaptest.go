178c2d774ae6027b23c8be219dac9775bccb8278
“robinhzhang” <1137831420@qq.com>
2022-05-12 12:31:28 +0800

    --study 依赖管理gopath govendor

---
+++ yilai/zaptest.go
@@ -0,0 +1,13 @@
+package yilai
+
+//
+//import "go.uber.org/zap"
+//
+//func main() {
+//	log, _ := zap.NewProduction()
+// 	log.Warn("warn")
+//
+//	//gopath
+//	//多版本依赖 govendor 诞生了很多第三方包管理工具
+//	//gomod出现
+//}
