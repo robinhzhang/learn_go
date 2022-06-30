9fa41823e49841828558763679a0b44e0493c50e
“robinhzhang” <1137831420@qq.com>
2022-06-30 17:31:50 +0800

    --整理前段学习，沉淀成文档

--- 213interface_duck/mock/mock_retriever.go
+++ 213interface_duck/mock/mock_retriever.go
@@ -1,9 +1,22 @@
 package mock
 
+import "fmt"
+
 type Retriever struct {
 	Content string
 }
 
 func (r Retriever) GetURLContent(url string) string {
 	return r.Content
+}
+
+func (r *Retriever) Post(url string, form map[string]string) string {
+	//注意指针接受者
+	r.Content = form["content"]
+	return "ok"
+}
+
+//类似于tostring（）方法
+func (r Retriever) String() string {
+	return fmt.Sprintf("Retriever:{content=%s}", r.Content)
 }
